367377c6a517a630ab1b566cb936a86aaca90efd
vasya_k <vasya_kukarsev@mail.ru>
2024-01-13 18:31:33 +0300

    Создан апи в переменной окружения, даписан channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,23 @@
-
+import os
+import json
+from googleapiclient.discovery import build
+import isodate
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        api_key: str = os.getenv('API_KEY_YOUTUBE')
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
+
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        response = self.youtube.channels().list(
+            id=self.channel_id,
+            part='snippet,statistics'
+        ).execute()
+        print(json.dumps(response, indent=2, ensure_ascii=False))
