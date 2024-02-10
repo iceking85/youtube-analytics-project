4eae0e915c20040b01136f39343300af0b906ca3
vasya_k <vasya_kukarsev@mail.ru>
2024-02-10 22:59:05 +0300

    Создан класс плейлист и описаны методы для него

--- src/video.py
+++ src/video.py
@@ -8,19 +8,25 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 class Video:
     def __init__(self, video_id):
         self.__video_id = video_id
-        self.video_data = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+        try:
+            self.video_data = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                id=video_id).execute()
-        self.video_title = self.video_data['items'][0]['snippet']['title']
-        self.video_url = f'https://www.youtube.com/watch?v={self.__video_id}'
-        self.views = self.video_data['items'][0]['statistics']['viewCount']
-        self.likes = self.video_data['items'][0]['statistics']['likeCount']
+            self.title = self.video_data['items'][0]['snippet']['title']
+            self.video_url = f'https://www.youtube.com/watch?v={self.__video_id}'
+            self.views = self.video_data['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_data['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.video_url = None
+            self.views = None
+            self.like_count = None
 
     @property
     def video_id(self):
         return self.__video_id
 
     def __str__(self):
-        return f"{self.video_title}"
+        return f"{self.title}"
 
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
